d347d9a425fa26b1fcce4ce2afd2086f98aa8da5
Kenneth Zamora <kmzamora@uamv.edu.ni>
2025-09-22 10:52:21 -0600

    hasta aca legue :/ me quede en la importacion

---
+++ Producto/Dao/ProductoDao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Producto.Modelos;
+
+
+namespace Producto.Dao
+{
+    internal class ProductoDao
+    {
+        
+        private Producto[] productos;
+    }
+}
